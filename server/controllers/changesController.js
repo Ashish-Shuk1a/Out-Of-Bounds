424a5163876fe898b7fa56bde749cb134eb9dc4a
Ashish <ashish763shukla@gmail.com>
2023-10-07 13:02:42 +0530

    minor changes

--- server/controllers/changesController.js
+++ server/controllers/changesController.js
@@ -35,7 +35,38 @@ const addEventScope = async (req, res) => {
     }
 }
 
+const addAbout = async(req,res)=>{
+    const user = await User.updateMany({}, { about: "This is a sample about"},{new:true})
+    return res.status(200).json({
+        "status": "success",
+        "message": "About updated successfully",
+        "data": user
+    })
+}
+
+const addZipCode = async(req,res)=>{
+    const user = await User.updateMany({},{location:{region:"Malad",city:"Mumbai",state:"Maharashtra",country:"India",zip_code:"400064"}})
+    const event = await Event.updateMany({},{location:{region:"Malad",city:"Mumbai",state:"Maharashtra",country:"India",zip_code:"400064"}})
+    const petition = await Petition.updateMany({},{location:{region:"Malad",city:"Mumbai",state:"Maharashtra",country:"India",zip_code:"400064"}})
+    return res.status(200).json({
+        "status": "success",
+        "message": "Zip Code updated successfully"
+    })
+}
+
+const changeEvent = async(req,res)=>{
+    const event = await Event.updateMany({},{video:null,open:true,res_req:false,response:{clip_req:false,questions:[]},questions:null},{new:true})
+    return res.status(200).json({
+        "status": "success",
+        "message": "Event updated successfully",
+        "data": event
+    })
+}
+
 module.exports = {
     editLocation,
-    addEventScope
+    addEventScope,
+    addAbout,
+    addZipCode,
+    changeEvent
 }
